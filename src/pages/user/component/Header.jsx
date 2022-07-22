e43519702d5812cf33a7fee84134d1ece0d213f1
AtlanticNova <69181762+AtlanticNova@users.noreply.github.com>
2022-07-23 02:39:03 +0700

    Update Component

--- src/pages/user/component/Header.jsx
+++ src/pages/user/component/Header.jsx
@@ -1,35 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button, Stack, } from '@chakra-ui/react'
 
 function Header() {
   return (
     <header className="header-area header-sticky">
-      <div className="container mt-2">
+      <div className="container">
         <div className="row">
           <div className="col-12">
             <nav className="main-nav">
-              {/* <!-- ***** Logo Start ***** --> */}
-              <a href="index.html" className="logo mt-4">
+              <a href="/" className="logo pt-4">
                 <h5>CLOOTH</h5>
               </a>
-              {/* <!-- ***** Logo End ***** --> */}
-              {/* <!-- ***** Menu Start ***** --> */}
-              <ul className="nav">
-                <li className="scroll-to-section">
-                  <Link to={"/"}>Home</Link>
-                </li>
-                <li className="scroll-to-section">
-                  <Link to={"/products"}>Products</Link>
-                </li>
-                <li className="scroll-to-section"><a href="about.html">About Us</a></li>
-
-                <li className="scroll-to-section btn btn-outline-info ml-5"><a href="#">Register</a></li>
-                <li className="scroll-to-section btn btn-outline-success ml-2 px-3"><a href="contact.html">Login</a></li>
+              <ul align='center' className="nav">
+                <li><Link to={"/"}>Home</Link></li>
+                <li><Link to={"/products"}>Products</Link></li>
+                <li><a href="/about">About Us</a></li>
+                <Stack pl={4} direction='row' spacing={4}>
+                  <Link to={"/register"}>
+                    <Button colorScheme='gray' variant='outline'>Register</Button>
+                  </Link>
+                  <Link to={"/login"}>
+                    <Button colorScheme='gray'>Login</Button>
+                  </Link>
+                </Stack>
               </ul>
               <a className='menu-trigger'>
                 <span>Menu</span>
               </a>
-              {/* <!-- ***** Menu End ***** --> */}
             </nav>
           </div>
         </div>
